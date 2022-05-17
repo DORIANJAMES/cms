8970b913f243ff75fb3f65543d6a9810f31faeda
DORIAN <dorian@t1000robotics.com>
2022-05-17 17:21:40 +0300

    Eklenen ürünler için resim işlemleri formu yapıldı. Bu form altında yüklenen resimlerin listeleneceği ve işlemlerinin yapılacaığı tablo oluşturuldu.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -55,6 +55,7 @@
                                         <i class="fa fa-trash"></i> Sil
                                     </button>
                                     <a href="<?php echo base_url("product/update_form/$item->id"); ?>" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</a>
+                                    <a href="<?php echo base_url("product/image_form/$item->id"); ?>" class="btn btn-sm btn-warning btn-outline"><i class="fa fa-image"></i> Resimler</a>
                                 </td>
                             </tr>
 
