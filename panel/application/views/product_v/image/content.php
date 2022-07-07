f1e4d7c8b2af1de5c733cdbb3b65aab8621a99ad
Alihan AÇIKGÖZ <dorianthelycan@gmail.com>
2022-07-07 12:27:51 +0300

    Ürün resim ekleme ve listeleme kısmı tamamlandı

--- panel/application/views/product_v/image/content.php
+++ panel/application/views/product_v/image/content.php
@@ -27,40 +27,58 @@
     <div class="col-md-12">
         <div class="widget">
             <div class="widget-body">
-                <table class="table table-hover table-bordered">
-                    <thead>
+                <?php
+                if (empty($item_images)) { ?>
+                    <div class="alert alert-info text-center">
+                        <p>Bu ürün altında herhangi bir resim bulunmamaktadır. Yukarıdaki yükleme alanından resim ekleyebilirsiniz...</p>
+                    </div>
+                <?php } else { ?>
+
+                    <table class="table table-hover table-bordered">
+                        <thead>
                         <th class="w100 text-center">#id</th>
                         <th class="w100 text-center">Görsel</th>
                         <th class="text-center">Resim Adı</th>
                         <th class="w100 text-center">Durumu</th>
                         <th class="w100 text-center">İşlem</th>
 
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td class="text-center">#1</td>
-                            <td class="text-center"><img width="30" class="img-responsive" src="https://yt3.ggpht.com/ytc/AKedOLSj2FK7hgQqQZh3yP73icIUBHrElm1blSMpOsHV=s176-c-k-c0x00ffffff-no-rj" alt=""></td>
-                            <td class="text-center">deneme-urunu.jpeg</td>
-                            <td class="text-center">
-                                <input
-                                    data-url="<?php echo base_url("product/isActiveSetter") ?>" 
-                                    type="checkbox"
-                                    class="isActive"
-                                    data-switchery=""
-                                    data-color="#10c469"
-                                    <?php echo (true) ? "checked":"";?>
-                                >
-                            </td>
-                            <td class="text-center">
-                                <button 
-                                    data-url="<?php echo base_url("product/delete/"); ?>" 
-                                    class="btn btn-sm btn-danger btn-outline remove-btn btn-block">
-                                    <i class="fa fa-trash"></i> Sil
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+
+                        <?php foreach ($item_images as $image) { ?>
+                            <tr>
+                                <td class="text-center">#<?php echo $image->id ?> </td>
+                                <td class="text-center"><img class="img-responsive img-circle w30" src="<?php echo base_url("uploads/{$viewFolder}/".$image->img_url) ?> " alt="<?php echo $image->img_url; ?> "></td>
+                                <td class="text-center"><?php echo $image->img_url ?> </td>
+                                <td class="text-center">
+                                    <input
+                                            data-url="<?php echo base_url("product/isActiveSetter") ?>"
+                                            type="checkbox"
+                                            class="isActive"
+                                            data-switchery=""
+                                            data-color="#10c469"
+                                        <?php echo (true) ? "checked":"";?>
+                                    >
+                                </td>
+                                <td class="text-center">
+                                    <button
+                                            data-url="<?php echo base_url("product_image/delete/$image->id"); ?>"
+                                            class="btn btn-sm btn-danger btn-outline remove-btn btn-block">
+                                        <i class="fa fa-trash"></i> Sil
+                                    </button>
+                                </td>
+                            </tr>
+                        <?php } ?>
+
+
+
+
+
+                        </tbody>
+                    </table>
+
+                <?php } ?>
+
             </div><!-- .widget-body -->
         </div><!-- .widget -->
     </div><!-- END column -->
